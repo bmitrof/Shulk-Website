585f5b4b0a9d93fe1ebc79bc5c80550d5490237c
unknown <bmitrof@yahoo.com>
2015-03-06 18:23:56 -0800

    added all files

---
+++ soundmanager.js
@@ -0,0 +1,35 @@
+//load sounds at start of webpage
+function loadSounds() {
+	if (!createjs.Sound.initializeDefaultPlugins()) {
+		window.alert("problem loading sounds");
+		return;
+	}
+	
+	var sounds = [
+		{src: "feelingit.ogg", id: "feels"},
+		{src: "gaurplains.mp3", id: "gaurplains"}
+	];
+	createjs.Sound.alternateExtensions = ["mp3"];
+	
+	for (var i = 0; i < sounds.length; i++) {
+		createjs.Sound.registerSound(sounds[i].src, sounds[i].id, 50);
+	}
+}
+
+var numberoffeels = 0;
+function displayFeels() {
+	numberoffeels ++;
+	document.getElementById("counter").innerHTML = "FEELS: " + numberoffeels;
+}
+
+var firstplay = true;
+function playSound() {
+	if (firstplay) {
+		createjs.Sound.play("gaurplains", "none", 0, 0, 0, .75);
+		firstplay = false;
+	}
+	createjs.Sound.play("feels", "none", 0, 0, 0, 1);
+	displayFeels();
+}
+
+
